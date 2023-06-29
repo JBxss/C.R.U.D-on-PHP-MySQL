ab1fc3770159a23ecff300d1594cc2915d1fb850
Juan Bossa <jbxss@MacBook-Pro-de-Juan.local>
2023-06-28 22:28:50 -0500

    Acciones de editar y eliminar agregadas

--- delete_task.php
+++ delete_task.php
@@ -4,7 +4,7 @@ include('database.php');
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-    $query = 'DELETE FROM task WHERE id = $id';
+    $query = "DELETE FROM task WHERE id = $id";
     $result = mysqli_query($conn, $query);
     if(!$result) {
         die('Query Failed!');
---
+++ edit_task.php
@@ -0,0 +1,42 @@
+<?php
+
+include('database.php');
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $query = "SELECT * FROM task WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_array($result);
+        $title = $row['title'];
+        $description = $row['description'];
+    }
+
+}
+
+?>
+
+<?php include("includes/header.php")?>
+
+<div class="container p-4">
+    <div class="row">
+        <div class="col-md-4 mx-auto">
+            <div class="card card-body">
+                <form action="">
+                    <div class="form-group pb-3">
+                        <input type="text" name="title" value="<?= $title ?>" class="form-control" placeholder="Update Title">
+                    </div>
+                    <div class="form-group pb-3">
+                        <textarea name="description" rows="3" class="form-control" placeholder="Update Description"><?= $description ?></textarea>
+                    </div>
+                    <button class="btn btn-success mx-auto" name="update">
+                        Update
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("includes/footer.php")?>
